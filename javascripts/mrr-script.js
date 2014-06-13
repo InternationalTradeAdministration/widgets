f7f71e1a6a6b73469348b153142e13be9ebd1566
AJ <aj@govwizely.com>
2014-06-12 22:47:12 -0400

    update

--- javascripts/mrr-script.js
+++ javascripts/mrr-script.js
@@ -33,7 +33,7 @@
 				industry = industryList[industryIndex];
 			}
 			if (countryIndex > 0){
-				country = countryList[countryIndex][1]
+				country = countryList[countryIndex];
 			}
 			var searchParams = "country=" + country + "&industry=" + industry;
 		}
@@ -92,8 +92,8 @@
 	      
 				form += ('<p><div class="select-input"><select class="search-input" id="mrr-industry"></select>');
 				form += ('<button class="search-button" id="industry-mrr-button"></button></div></p>');
-				form += ('<div class="select-input"><select class="search-input" id="mrr-country"></select>');
-				form += ('<button class="search-button" id="country-mrr-button"></button></div>');
+				form += ('<p><div class="select-input"><select class="search-input" id="mrr-country"></select>');
+				form += ('<button class="search-button" id="country-mrr-button"></button></div></p>');
 				document.getElementById('mrr-form').innerHTML = form;
 				$('#industry-mrr-button').on('click', function(){
 					$(this).addClass('search-button-clear');
@@ -110,8 +110,8 @@
 				$.each(industryList, function(val, text) {
 		      $('#mrr-industry').append( $('<option></option>').val(val).html(text));
 		     });
-				$.each(countryList, function(val, array) {
-		      $('#mrr-country').append( $('<option></option>').val(val).html(array[0]));
+				$.each(countryList, function(val, text) {
+		      $('#mrr-country').append( $('<option></option>').val(val).html(text));
 		     });
 	    });
 	}
