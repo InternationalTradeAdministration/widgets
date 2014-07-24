80a3112827e2e963e1da6c01f7e4504c9099bec6
AJ Singh <ajsingh273@gmail.com>
2014-07-24 13:59:18 -0400

    update

--- _site/javascripts/articles-script.js
+++ _site/javascripts/articles-script.js
@@ -25,7 +25,7 @@
 			url += keyword;
 		}
 		else {
-			alert("No article search term entered");
+			alert("No article keyword entered");
 			document.getElementById("articles-results").innerHTML = "";
 			stopSpinner(spinner);
 			return;
@@ -37,7 +37,7 @@
 			success: function(feed){
 				var results = feed.results;
 				if (results.length == 0){
-					list = "<p>No articles were found, please try another search term.<p>"
+					list = "<p>No articles were found, please try another keyword.<p>"
 				}
 				else {
 					var list = "<p class='results-title'></p>";
@@ -81,7 +81,7 @@
 			$('#articles-container').addClass('widget-container');
 			var form = "";				
       
-			form += ('<div class="search-combo"><input class="search-input" type="text" id="articles-keyword" placeholder="Enter a search term" size="40">');
+			form += ('<div class="search-combo"><input class="search-input" type="text" id="articles-keyword" placeholder="search by keyword" size="30">');
 			form += ('<button class="search-button" id="articles-button"></button></div>');
 			document.getElementById('articles-form').innerHTML = form;
 			$('#articles-button').on('click', function(){
