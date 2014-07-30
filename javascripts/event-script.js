b68b0a4d8b7e394034864d9f17bdddbd3d7a512a
AJ Singh <ajsingh273@gmail.com>
2014-07-30 10:02:31 -0400

    update

--- javascripts/event-script.js
+++ javascripts/event-script.js
@@ -95,7 +95,7 @@
 				form += ('<button class="search-button2" id="industry-event-button"></button></div></p>');
 				form += ('<p></p><div class="select-input"><select class="search-input" id="event-countries"></select>');
 				form += ('</br></br><button class="search-button3" id="countries-event-button">Filter</button></div>');
-				form += ('<div><form id="myForm"><input type="button" class="search-button3" onclick="myFunction()" value="Reset"></form></form></div>');
+				form += ('<div><form id="myForm"><input type="button" class="search-button3" onclick="myFunction()" value="Reset"></form></form><script>function myFunction(){document.getElementById("myForm").reset();}</script></div>');
 				
 				
 				document.getElementById('event-form').innerHTML = form;
